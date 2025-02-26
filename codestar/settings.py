fb88946ce16085b4f45504a3916213c44b180ef2
Ali Gate <ali_also@yahoo.es>
2025-02-26 16:25:49 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^2p4&s43$qt@xeo&)&**%vst34oeifj8s8cez+1gm=ji+f6oo0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/','.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
