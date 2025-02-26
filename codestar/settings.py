4231c3c54ce97e9c0945aaa5d52662f233919472
Ali Gate <ali_also@yahoo.es>
2025-02-26 16:21:35 +0000

    created heroiku app , installed gunicorn and created Procfile. added heroku and local host to alloed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^2p4&s43$qt@xeo&)&**%vst34oeifj8s8cez+1gm=ji+f6oo0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/','.herokuapp.com']
 
 
 # Application definition
